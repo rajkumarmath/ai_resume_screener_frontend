fdef6aef1bb85818e635cfd1dcb594c3ddb0eec3
Rajkumar Math <rajkumarmath21@gmail.com>
2026-02-17 23:22:28 +0530

    Refactor login page component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -63,9 +63,10 @@ export default function LoginPage() {
           </button>
         </form>
         <p className="mt-4 text-center text-sm text-gray-600">
-          Don't have an account? <Link href="/register" className="text-blue-600 hover:underline">Register</Link>
+          Don&apos;t have an account? <Link href="/register" className="text-blue-600 hover:underline">Register</Link>
         </p>
       </div>
     </div>
   );
+
 }
